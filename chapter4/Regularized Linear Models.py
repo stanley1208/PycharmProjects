3a47f2ccfade7f3084af6c8506bc5d39a7275ac6
A8250092 <A8250092@g.pccu.eddu.tw>
2022-05-27 11:33:51 +0800

    update

--- chapter4/Regularized Linear Models.py
+++ chapter4/Regularized Linear Models.py
@@ -6,6 +6,9 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import SGDRegressor
+from sklearn.linear_model import Lasso
+from sklearn.linear_model import ElasticNet
+
 # Ridge Regression
 m=20
 X=3*np.random.rand(m,1)
@@ -39,17 +42,38 @@ def plot_model(model_class, polynomial, alphas, **model_kargs):
     plt.axis([0,3,0,4])
 
 
-plt.figure(figsize=(8,4))
-plt.subplot(121)
-plot_model(Ridge, polynomial=False, alphas=(0,10,100), random_state=42)
-plt.ylabel("$y$",rotation=0,fontsize=18)
-plt.subplot(122)
-plot_model(Ridge, polynomial=True, alphas=(0,10**-5,1), random_state=42)
+# plt.figure(figsize=(8,4))
+# plt.subplot(121)
+# plot_model(Ridge, polynomial=False, alphas=(0,10,100), random_state=42)
+# plt.ylabel("$y$",rotation=0,fontsize=18)
+# plt.subplot(122)
+# plot_model(Ridge, polynomial=True, alphas=(0,10**-5,1), random_state=42)
 # plt.show()
 
 # sgd_reg = SGDRegressor(penalty="l2", max_iter=1000, tol=1e-3, random_state=42)
 # sgd_reg.fit(X, y.ravel())
 # print(sgd_reg.predict([[1.5]]))
 
+# Lasso Regression
+# plt.plot(figsize=(8,4))
+# plt.subplot(121)
+# plot_model(Lasso,polynomial=False,alphas=(0,0.1,1),random_state=42)
+# plt.ylabel("$y$",rotation=0,fontsize=18)
+# plt.subplot(122)
+# plot_model(Lasso,polynomial=True,alphas=(0,10**-7,1),random_state=42)
+# plt.show()
+
+# lasso_reg=Lasso(alpha=0.1)
+# lasso_reg.fit(X,y)
+# print(lasso_reg.predict([[1.5]]))
+#
+# sgd_reg = SGDRegressor(penalty="l1", max_iter=1000, tol=1e-3, random_state=42)
+# sgd_reg.fit(X, y.ravel())
+# print(sgd_reg.predict([[1.5]]))
+
+# Elastic Net
+# elastic_net=ElasticNet(alpha=0.1,l1_ratio=0.5)
+# elastic_net.fit(X,y)
+# print(elastic_net.predict([[1.5]]))
 
 
